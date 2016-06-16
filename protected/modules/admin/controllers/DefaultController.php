fca7f43cd5ebf60bb0aff89b857d75058a91ce24
Karthiga <go2karthi9@gmail.com>
2016-06-16 18:05:38 +0530

    tet

--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -2,8 +2,11 @@
 
 class DefaultController extends Controller
 {
+	 // public $layout=false;
 	public function actionIndex()
 	{
+		$this->layout = false;
+		// echo md5(123);
 		$this->render('index');
 	}
 }
